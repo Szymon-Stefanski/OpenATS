5d9456104ca570eacad9e361423f443852fe7620
Szymon Stefański <szymonstefanski1@gmail.com>
2026-03-09 21:51:27 +0100

    Register models in Django admin panel

--- jobs/admin.py
+++ jobs/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import JobOffer, Candidate
 
-# Register your models here.
+@admin.register(JobOffer)
+class JobOfferAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_at')
+
+@admin.register(Candidate)
+class CandidateAdmin(admin.ModelAdmin):
+    list_display = ('full_name', 'job_offer', 'match_score')
